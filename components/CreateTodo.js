ba57ef67449ce1fc2009cdc640aaa67d7e6b45f2
Benjamin Davis <bmdavis419@protonmail.com>
2022-02-09 14:29:17 -0500

    created project

---
+++ components/CreateTodo.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+export default function CreateTodo(props) {
+  const { addTodo } = props;
+
+  const [newTodo, setNewTodo] = useState({
+    name: "",
+    due: "",
+    details: "",
+  });
+
+  const fSubmit = (e) => {
+    e.preventDefault();
+    addTodo(newTodo);
+  };
+
+  return (
+    <form className="card p-3" onSubmit={fSubmit}>
+      <h3 className="text-primary text-center">Create New Todo</h3>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name of Todo
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          onChange={(e) => {
+            setNewTodo({ ...newTodo, name: e.target.value });
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="due" className="form-label">
+          Due Date
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          id="due"
+          onChange={(e) => {
+            setNewTodo({ ...newTodo, due: e.target.value });
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="details" className="form-label">
+          Details
+        </label>
+        <textarea
+          className="form-control"
+          id="details"
+          rows="3"
+          onChange={(e) => {
+            setNewTodo({ ...newTodo, details: e.target.value });
+          }}
+        ></textarea>
+      </div>
+      <button type="submit" className="btn btn-primary">
+        Create
+      </button>
+    </form>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import CreateTodo from "../components/CreateTodo";
+import Todo from "../components/Todo";
+
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => setTodos([...todos, todo]);
+
+  const removeTodo = (idx) => {
+    let curTodos = todos;
+    curTodos.splice(idx, 1);
+    setTodos([...curTodos]);
+  };
+
+  return (
+    <div>
+      <h1 className="text-primary text-center">Week 5 Todo App</h1>
+      <div className="container">
+        {todos.map((todo, idx) => {
+          return (
+            <Todo todo={todo} idx={idx} removeTodo={removeTodo} key={idx} />
+          );
+        })}
+        <CreateTodo addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
